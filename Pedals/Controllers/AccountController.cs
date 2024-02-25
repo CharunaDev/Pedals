bfb06ad0162ef1ce9fb33541f59b71a88dce3749
Charuna Amarasinghe <charuna.amarasinghe@qualitapps.com>
2024-02-25 10:55:05 +0530

    second commit

---
+++ Pedals/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pedals.Models;
+
+
+namespace Pedals.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // Use IDisposable pattern to properly dispose of resources
+     
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Verify(Account acc)
+        {
+              try
+              {
+                connectionString();
+                con.Open();
+                cm.Connection = con;
+                cm.CommandText = "select * from sales.logins where username=@username and password = @password";
+                cm.Parameters.AddWithValue("@username", acc.username);
+                cm.Parameters.AddWithValue("@password", acc.password);
+                dr = cm.ExecuteReader();
+                if (dr.Read())
+                {
+                    con.Close();
+                    return View("Create"); // Return a success view
+                }
+                else
+                {
+                    con.Close();
+                    return View("Error"); // Return an error view
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception
+                return View("Error"); // Return an error view
+            }
+            finally
+            {
+                con.Close(); // Ensure connection is closed even if an exception occurs
+            }
+        }
+
+        void connectionString()
+        {
+            string connectionString = "Data Source=LAPTOP-5QSPL81Q\\FROZTY;Initial Catalog=Pedals;User ID=sa;Password=1234;MultipleActiveResultSets=True;App=EntityFramework";
+            con.ConnectionString = connectionString;
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Pedals/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pedals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Pedals")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8692d107-f3a2-40f6-ac08-0e1f2bc384e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
